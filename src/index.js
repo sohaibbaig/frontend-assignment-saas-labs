6b0d01157f617538d33354c3d53eeae6b11bb0f7
Sohaib Baig <sohaib.baig@WFX-LAP-1175.local>
2024-12-14 00:24:26 +0530

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
+
+function App() {
+  return (
+    <div>
+      <h1>Sohaib</h1>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'output.js'                 
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:  'babel-loader'
+            }
+        ]
+    },
+    devServer: {
+        static: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3000,
+        hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    })
+  ]
+};
